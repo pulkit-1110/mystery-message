1b1b0e29d68c112c05438700b4f7da06b9f11658
Pulkit Uppal <pulkituppal@Pulkits-MacBook-Air.local>
2025-02-11 13:22:55 +0530

    added default strings for env variables

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -66,7 +66,7 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || 'My next auth secret',
   pages: {
     signIn: '/sign-in',
   },
--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -11,8 +11,8 @@ export async function sendVerificationEmail(
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
+        user: process.env.EMAIL_USERNAME || 'email id',
+        pass: process.env.EMAIL_PASSWORD || 'password',
       },
     })
 
